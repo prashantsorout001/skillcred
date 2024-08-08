67e0dfdaf0308ae5a68cb5b439db418511d0045c
prashantsorout001 <146057974+prashantsorout001@users.noreply.github.com>
2024-08-08 12:24:50 +0530

    Add files via upload

---
+++ skillcred/chat_bot.py
@@ -0,0 +1,47 @@
+import streamlit as st #for ui
+import os
+from dotenv import load_dotenv #to get env variables loaded into the application
+load_dotenv() #loading of all the env variable
+
+import google.generativeai as genai
+history=[]
+
+#genai config of api
+
+genai.configure(api_key="AIzaSyBcTO9SR4QnCSO8gaPWesQof6Ctg7ihcaA")
+# initialise the model
+model = genai.GenerativeModel('gemini-pro')
+
+# define a function to generate response from llm
+def get_gemini_response(ques):
+    resp = model.generate_content(ques)
+    return resp.text
+
+# setting up streamlit app
+st.set_page_config(
+    page_title="Gemini pro Q/A project",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+# setting up header
+st.header("Gemini Q/A app")
+
+
+# input
+question = st.text_input("Ask a question: ")
+
+#submit button
+if st.button("Submit"):
+    response = get_gemini_response(question)
+    st.write("*User*:", question)
+    st.write("*Bot*:", response)
+    history.append((question, response))
+
+
+st.sidebar.title("Chat History")
+for i, (user_q, bot_resp) in enumerate(history[::-1], start=1):
+    st.sidebar.write(f"{i}. User: {user_q}")
+    st.sidebar.write(f"   Bot: {bot_resp}")
+
+
